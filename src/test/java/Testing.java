bdb6c5f7d1161e09aebf84851eefd81863198105
moustd <dionmoustos@gmail.com>
2020-02-09 19:12:59 +1030

    Cleanup (#3)
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup

---
+++ src/test/java/Testing.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Testing {
+
+
+    @Test
+    public void evaluatesExpression() {
+        System.out.println("Begin Test");
+        assertEquals(1, 1);
+    }
+}
